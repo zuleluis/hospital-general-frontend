0fc4f8f15b114eeba182f00a8a489177c928154a
MarcoAntonioAS17 <antonioarcosmarco@gmail.com>
2021-08-04 20:49:45 -0500

    Problemas resueltos de deploy

--- hospital-general/src/views/pacientes/paginas/historiaClinica/formularioStyles.js
+++ hospital-general/src/views/pacientes/paginas/historiaClinica/formularioStyles.js
@@ -41,7 +41,7 @@ const useStyles = makeStyles((theme) => ({
       justifyContent: "center",
     },
     radioGroup: {
-      display: "block",
+      display: "block !important",
     },
     radioButton: {
       justifyContent: "center",
