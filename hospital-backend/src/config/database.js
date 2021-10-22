73eaed004c479d13a55b3551a8283447d72c17ae
Zule Luis <zulyyy123@gmail.com>
2021-10-21 19:25:49 -0500

    Front end exclusivo

--- hospital-backend/src/config/database.js
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from "sequelize";
- 
-const db = new Sequelize('hospitalGeneral_BocaDelRio', 'root', '', {
-    host: "localhost",
-    dialect: "mysql"
-});
- 
-export default db;
--- hospital-general/src/routes/pacientesRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const pacientesController = require('../controllers/pacientesController')
-
-router.get('/', pacientesController.index);
-
-module.exports = router;
